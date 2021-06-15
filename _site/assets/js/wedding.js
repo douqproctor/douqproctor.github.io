10a42027161d882050f9cae129e9dce50e8b000b
Doug Proctor <dougproctor@gmail.com>
2021-06-15 13:40:46 -0600

    updating to reflect k&d2020.com changes

---
+++ _site/assets/js/wedding.js
@@ -0,0 +1,22 @@
+function markFormAsSubmitted() {
+  var form = document.getElementById('rsvp-form');
+  if (!form) {
+    return;
+  }
+  form.classList.add('submitted');
+}
+
+function makePhotosVisible() {
+  var photos = document.querySelectorAll("#photo-container img");
+  for (var i=0; i<photos.length; i++) {
+    photos[i].style["opacity"] = 1;
+  }
+}
+
+window.addEventListener('load', function() {
+  var menuButton = document.getElementById('mobile-nav-container');
+  menuButton.onclick = function() {
+    menuButton.classList.toggle('menu-active');
+  };
+  makePhotosVisible();
+});
